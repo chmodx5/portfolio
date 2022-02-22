314a34eca186395aea5dfd6340ae1ddc25d94604
chmodx5 <chmodx5@gmail.com>
2022-02-22 12:10:38 +0300

    fixing layout import

--- pages/Portfolio.jsx
+++ pages/Portfolio.jsx
@@ -3,8 +3,6 @@ import IsotopeGrid from "../components/Blocks/Portfolio/IsotopeGrid";
 import Breadcrumbs from "../components/Blocks/Breadcrumbs/Breadcrumbs";
 
 export default function Portfolio() {
-  //getting categories from items and filtering them to ensure there are no duplicates
-
   return (
     <>
       <div>
